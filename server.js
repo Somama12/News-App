d04772df89333a1b27dd4167c050867f37b24c34
Somama Siddiqui <146497258+Somama12@users.noreply.github.com>
2024-02-17 17:03:10 -0500

    Add files via upload

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const { dirname } = require('path')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.sendFile("index.html",{root:__dirname})
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
